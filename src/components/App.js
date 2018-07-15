3ea3bb67235c77ae273eeda07f444c9da92e2af5
Joshua Jackson <washingtonfaulconer@gmail.com>
2018-07-15 17:41:13 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import styles from './App.scss';
+
+class App extends React.Component 
+{
+  render()
+  {
+    return (
+      <div className={styles.app}>
+
+      </div>
+    );
+  }
+}
+
+export default App;
