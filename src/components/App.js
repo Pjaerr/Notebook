3b2a6390037fc9d6bd5dc16425b91f4d662638ad
Joshua Jackson <washingtonfaulconer@gmail.com>
2018-07-28 10:10:15 +0100

    Render list of Note components based on given notes text
    Text is stored in state as an array of strings, a note component is rendered for each string and if no notes exist it will just show that nothing exists

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import styles from './App.scss';
 
+import Notes from './Notes';
+
 class App extends React.Component 
 {
   render()
   {
     return (
       <div className={styles.app}>
-
+        <Notes />
       </div>
     );
   }
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,13 +2,35 @@ import React from 'react';
 
 import styles from './Notes.scss';
 
+import Note from './Note';
+
 class Notes extends React.Component 
 {
+    state =
+        {
+            notes: [] //Should just be text and then rendered as a jsx component
+        };
+
+    renderNotes = () =>
+    {
+        const { notes } = this.state;
+
+        if (notes.length > 0)
+        {
+
+            return notes.map(text => <Note text={text} />);
+        }
+        else
+        {
+            return <p>Nothing to see here 😎</p>
+        }
+    }
+
     render()
     {
         return (
             <div className={styles.notes}>
-
+                {this.renderNotes()}
             </div>
         );
     }
