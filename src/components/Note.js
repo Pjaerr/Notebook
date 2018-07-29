b1fb07a38e073d90375a273c58d1814536cd36a1
Josh Jackson <joshua.j@mmtdigital.co.uk>
2018-07-29 11:12:39 +0100

    Added proper delete button on notes and using <pre> now
    Now notes reflect what has actually been typed in because I am using <pre> tags

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,14 +2,39 @@ import React from 'react';
 
 import styles from './Note.scss';
 
-const Note = (props) =>
+class Note extends React.Component
 {
-    return (
-        <div className={styles.note}>
-            <p>{props.text}</p>
-            <button onClick={() => { props.deleteNote(props.id) }}>Delete</button>
-        </div>
-    );
+    state =
+        {
+            deleteButtonClass: `${styles.deleteButton}`
+        };
+
+    deleteIsActive = false;
+
+    handleClick = () =>
+    {
+        if (this.deleteIsActive)
+        {
+            this.setState({ deleteButtonClass: `${styles.deleteButton}` });
+        }
+        else
+        {
+            this.setState({ deleteButtonClass: `${styles.deleteButton} ${styles.active}` });
+        }
+
+
+        this.deleteIsActive = !this.deleteIsActive;
+    }
+
+    render()
+    {
+        return (
+            <div className={styles.note} onClick={this.handleClick}>
+                <button className={this.state.deleteButtonClass} onClick={() => { this.props.deleteNote(this.props.id) }}>Delete</button>
+                <pre>{this.props.text}</pre>
+            </div >
+        );
+    }
 }
 
 export default Note;
